7979738f407a0e908dd610fea1bc439904165c03
Antonio Sanchez <asanchez@2riderentalcr.com>
2025-04-23 22:15:35 -0600

    trabajo final

---
+++ Proyectos/Proyecto Sistema Ventas/main.py
@@ -0,0 +1,57 @@
+"""
+Autor: Antonio Sánchez
+Fecha: 23/4/2025
+Versión: 0.1
+Sistema de Gestión de Ventas 
+"""
+
+import os
+from modulo ingresar_ventas
+
+def ingresar_ventas(lista_ventas):
+    pass
+    
+
+def limpiar_pantalla():
+    """Limpia la pantalla de la terminal en ejecución"""
+    os.system("cls" if os.name == "nt" else "clear")
+    
+
+def pausar():
+    input("\n Presione enter para continuar")
+
+#Menú Principal
+def menu():
+    while True: 
+        print("\n---- Menú Principal ----")
+        print("1. Ingresar Ventas de cursos UMCA")
+        print("2. Guardar datos en un archivo CSV")
+        print("3. Analizar las ventas")
+        print("4. Salir")
+        opcion = input("Ingrese una Opcion: ")
+        
+        if opcion == "1" :
+            print("\n---- Ingreso de Ventas de Cursos----")
+            ingresar_ventas(ventas)
+            pausar()
+        elif opcion == "2":
+            print("\n----Guardar Ventas----")
+            pausar()
+        elif opcion == "3":
+            print("\n---- Análisis de Ventas----")
+            pausar()
+        elif opcion == "4":
+            print("\n Gracias por usar el sistema. Hasta pronto")  
+            pausar()
+            break # Con el break se cierra el sistema del ciclo while
+        else:
+            print("Opcion no valida. Intente Nuevamente una opcion.")          
+
+
+
+#Ejecución del sistema si solo si el archivo es el main
+if __name__ == "__main__" :
+    print ("Bienvenido al sistema de gestión de Ventas")
+    menu()
+    
+    
