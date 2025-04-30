08796989c0e1679df389e9077d134ea9f2c276ff
Antonio Sanchez <asanchez@2riderentalcr.com>
2025-04-29 22:57:03 -0600

    proyecto final

--- Proyectos/Proyecto Sistema Ventas/main.py
+++ Proyectos/Proyecto Sistema Ventas/main.py
@@ -1,56 +1,62 @@
 """
-Autor: Antonio Sánchez
-Fecha: 23/4/2025
+Autor: Anrtonio Sánchez
+Fecha: 2025-04-16
 Versión: 0.1
-Sistema de Gestión de Ventas 
+Sistema de Gestión de Ventas que nos permita ingresar, almacenar y analizar datos de ventas.
 """
-
 import os
-from modulo import ingresar_ventas
+from modulo import ingresar_ventas, guardar_ventas, analisis_ventas
 
-    
 
 def limpiar_pantalla():
     """Limpia la pantalla de la terminal en ejecución"""
-    os.system("cls" if os.name == "nt" else "clear")
-    
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def pausar():
-    input("\n Presione enter para continuar")
+
+    input('\nPresione Enter para continuar....')
 
 #Menú Principal
 def menu():
+    #Variables del sistema
     ventas = []
-    while True: 
-        print("\n---- Menú Principal ----")
-        print("1. Ingresar Ventas de cursos UMCA")
-        print("2. Guardar datos en un archivo CSV")
-        print("3. Analizar las ventas")
-        print("4. Salir")
-        opcion = input("Ingrese una Opcion: ")
+    while True:
+        print('\n---- Menú Principal ----')
+        print('1. Ingresar ventas de cursos UMCA')
+        print('2. Guardar datos en un archivo CSV')
+        print('3. Analizar las ventas')
+        print('4. Salir')
+        opcion = input('Ingrese una opción: ')
         
-        if opcion == "1" :
-            print("\n---- Ingreso de Ventas de Cursos----")
+        if opcion == "1":
+            limpiar_pantalla()
+            print('\n ---- Ingreso de Ventas de Cursos ----')
             ingresar_ventas(ventas)
+            print(ventas)
             pausar()
-        elif opcion == "2":
-            print("\n----Guardar Ventas----")
+        elif opcion == '2':
+            limpiar_pantalla()
+            print('\n---- Guardar Ventas en CSV ----')
+            guardar_ventas(ventas)
             pausar()
-        elif opcion == "3":
-            print("\n---- Análisis de Ventas----")
+        elif opcion == '3':
+            limpiar_pantalla()
+            print('\n ---- Analisis de Ventas ----')
+            analisis_ventas()
             pausar()
-        elif opcion == "4":
-            print("\n Gracias por usar el sistema. Hasta pronto")  
+        elif opcion == '4':
+            print('\n Gracias por usar el sistema. Hasta pronto!')
             pausar()
-            break # Con el break se cierra el sistema del ciclo while
+            break #<- Cierro el sistema deteniendo el ciclo while
         else:
-            print("Opcion no valida. Intente Nuevamente una opcion.")          
+            print('Opción no válida. Intente nuevamente una opción!')
+            pausar()
 
 
 
 #Ejecución del sistema si solo si el archivo es el main
-if __name__ == "__main__" :
-    print ("Bienvenido al sistema de gestión de Ventas")
+if __name__ == '__main__':
+    print('Bienvenido al sistema de Gestión de Ventas')
     menu()
     
     
